8b9194436f4091f7ea3560250002b3b732f22716
WilerMS <wilersoriano@gmail.com>
2024-02-25 16:29:07 +0100

    update eslint

--- platform/server/.eslintrc.js
+++ platform/server/.eslintrc.js
@@ -1,26 +1,30 @@
-module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
-    },
-    "extends": "standard-with-typescript",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
+const eslintrc = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: "standard-with-typescript",
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        ".eslintrc.{js,cjs}"
+      ],
+      parserOptions: {
+        sourceType: "script"
+      }
     }
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  rules: {
+    "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/await-thenable": "off"
+  }
 }
+
+export default eslintrc
